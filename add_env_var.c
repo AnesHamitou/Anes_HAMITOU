b90633acaf4a828a92533f48701221881d26f4e2
Anes HAMITOU <36543699+AnesHamitou@users.noreply.github.com>
2018-10-25 22:35:10 +0200

    correcting putenv error

--- add_env_var.c
+++ add_env_var.c
@@ -7,18 +7,18 @@ extern int errno;
 
 int main(int argc, char **argv){;
 	if(argc!=3){
-                printf("Usage : %s <VAR> <VALUE>\n",argv[0]);
-                exit(EXIT_FAILURE);
+		printf("Usage : %s <VAR> <VALUE>\n",argv[0]);
+        	exit(EXIT_FAILURE);
         }
-  int size = 2+strlen(argv[1])+strlen(argv[2]); //2 for '=' and '\0'
+	int size = 2+strlen(argv[1])+strlen(argv[2]); //2 for '=' and '\0'
 	char var[size];
 	strcat(var,argv[1]);
 	strcat(var,"=");
 	strcat(var,argv[2]);
-  if(!putenv(var)){
-    perror("putenv error");
-    exit(EXIT_FAILURE);
-  }
+  	if(putenv(var)){
+    		perror("putenv error");
+    		exit(EXIT_FAILURE);
+  	}
 	printf("Display :\n");
 	extern char **environ;
 	char **env=environ;
