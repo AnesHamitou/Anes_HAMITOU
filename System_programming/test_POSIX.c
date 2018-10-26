a9fe7678e0842ee944f44aed5db7cf088193b31f
Anes HAMITOU <hamitouanes@hotmail.com>
2018-10-26 20:50:04 +0200

    Adding POSIX constants

--- System_programming/test_POSIX.c
+++ System_programming/test_POSIX.c
@@ -1,9 +1,6 @@
-/*
- * example : gcc -D_POSIX_C_SOURCE=1993 -D_XOPEN_SOURCE=4  test_POSIX.c -o test_POSIX
- */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char **argv){
 	
@@ -18,5 +15,13 @@ int main(int argc, char **argv){
 	if(_XOPEN_SOURCE==1) printf("XPG3 used\n");
 	if(_XOPEN_SOURCE==4) printf("XPG4 used\n");
 
+	//Some sys constants
+	
+	printf("_SC_CHILD_MAX : %ld\n",sysconf(_SC_CHILD_MAX));
+	printf("_SC_HOST_NAME_MAX : %ld\n",sysconf(_SC_HOST_NAME_MAX));
+	printf("_SC_LOGIN_NAME_MAX : %ld\n",sysconf(_SC_LOGIN_NAME_MAX));
+	printf("_SC_OPEN_MAX : %ld\n",sysconf(_SC_OPEN_MAX));
+	printf("_SC_PAGESIZE : %ld\n",sysconf(_SC_PAGESIZE));
+	printf("_SC_VERSION : %ld\n",sysconf(_SC_VERSION));
 	exit(EXIT_SUCCESS);
 }
