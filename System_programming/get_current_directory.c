ca4ec010ddc436d6452d149e0475e5cfbb2850ae
Anes HAMITOU <hamitouanes@hotmail.com>
2018-10-26 22:04:21 +0200

    Update directory

---
+++ System_programming/get_current_directory.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+extern int errno;
+
+int main(int argc, char **argv){
+	char pwd[1024];
+	char *test;
+	if((test=getcwd(pwd,1024))==NULL){
+		perror("Getcwd error");
+		exit(EXIT_FAILURE);
+	}
+	printf("Current working directory : %s\n",pwd);
+	exit(EXIT_FAILURE);
+}
